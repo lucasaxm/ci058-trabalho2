31eb8063cfe70c80c70eafb57a1e6a14fca85d07
Lucas Affonso Xavier De Morais <lucas_axm@hotmail.com>
2014-05-08 00:27:12 -0300

    lendo de 4 em 4 bits

--- ham.rb
+++ ham.rb
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 binchars=[]
-input = File.open(ARGV[0], "r") do |file|
+input = File.open("input.txt", "r") do |file|
 	file.each_char do |c|
-		binchars << c.to_s(2)
+		string = "%08b" % c.ord
+		hi = string[0,4]
+		lo = string[4,7]
+		binchars << hi
+		binchars << lo
 	end
 end
 
-puts binchars.to_yaml
+puts binchars
