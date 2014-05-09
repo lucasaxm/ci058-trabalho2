0c2854709e33e9431ee47ef78f37493ae5566ded
Lucas Affonso Xavier De Morais <lucas_axm@hotmail.com>
2014-05-08 21:03:56 -0300

    codifica e decodifica funcionando. precisa de mais testes (Bonus: armazenar apenas 1 char ao inves de 8)

--- ham.rb
+++ ham.rb
@@ -16,7 +16,7 @@ def xor3Vetor (a)
 	return x
 end
 
-def converteArquivo(path)
+def converteArquivo4bits(path)
 	binchars=[]
 	input = File.open(path, "r") do |file|
 		file.each_char do |c|
@@ -30,11 +30,21 @@ def converteArquivo(path)
 	return binchars
 end
 
-def hamming47(vet4bits)
+def converteArquivo7bits(path)
+	vetor=[]
+	String.new
+	input = File.open(path, "r") do |file|
+		all=file.read;
+		vetor = all.split(' ')
+	end
+	return vetor
+end
+
+def codificaHamming47(vet4bits)
 	codificado = []
 	str = []
-	binarios = []
 	for i in 0..(vet4bits.length-1)
+		binarios = []
 		str[3]=vet4bits[i][0]
 		if str[3]=="1"
 			binarios << "%03b" % 3
@@ -55,12 +65,89 @@ def hamming47(vet4bits)
 		str[1] = paridade[2]
 		str[2] = paridade[1]
 		str[4] = paridade[0]
-
 		codificado << str.join
+	end
+	return codificado
+end
+
+def decodificaHamming47(vet7bits)
+	vet4bits=[]
+	vet7bits.each do |str7Bits|
+		str4Bits = String.new
+		binarios=[]
+		for i in 1..7
+			if str7Bits[i-1].to_i==1
+				binarios << "%03b" % i
+			end
+		end
+		bitErrado = xor3Vetor(binarios).to_i(2)
+		if bitErrado==0
+			str4Bits[0]=str7Bits[3-1]
+			str4Bits[1]=str7Bits[5-1]
+			str4Bits[2]=str7Bits[6-1]
+			str4Bits[3]=str7Bits[7-1]
+		else
+			# corrige
+		end
+		vet4bits << str4Bits
+	end
+	return vet4bits
+end
+
+def arrayToFile (array,path)
+	File.open(path, "w") { |file|
+		array.each do |x|
+			file.write (x+" ");
+		end
+	}	
+end
+
+def stringToFile (str,path)
+	File.open(path, "w") { |file|
+		file.write (str);
+	}	
+end
+
+def arrayToString (array, c)
+	str = String.new
+	i=0
+	j=i+c-1;
+	while i<(array.length-1)
+		str=str+(array[i..j].join.to_i(2).chr)
+		i=j+1
+		j=i+c-1
+	end
+	return str
+end
+
+def insereErro (vetor)
+	if ARGV.length>5
+		abort "Numero de parametros incorreto"
+	end
+	for i in 2..(ARGV.length-1)
+		if vetor[(ARGV[i].to_i)/7][(ARGV[i].to_i)%7]==1
+			vetor[(ARGV[i].to_i)/7][(ARGV[i].to_i)%7]="0"
+		else
+			vetor[(ARGV[i].to_i)/7][(ARGV[i].to_i)%7]="1"
+		end
 	end
 end
 
-vet4bits = []
-vet7bits = []
-vet4bits = converteArquivo("input.txt")
-vet7bits = hamming47(vet4bits)
+#main{
+	vet4bits = []
+	vet7bits = []
+	case ARGV[0]
+		when "-c"	
+			vet4bits = converteArquivo4bits("input.in")
+			vet7bits = codificaHamming47(vet4bits)
+			insereErro (vet7bits)
+			arrayToFile(vet7bits,"output.ham")
+		when "-d"
+			vet7bits = converteArquivo7bits("output.ham")
+			vet4bits = decodificaHamming47(vet7bits)
+			outputString = arrayToString(vet4bits,2)
+			stringToFile(outputString,"input.txt")
+		else
+			abort ("Parametro Incorreto (Use -c ou -d)")
+	end
+#}
